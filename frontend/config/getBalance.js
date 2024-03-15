0979483a3c82864f5a944c69d63cc749d961329d
GennW <778855@mail.ru>
2024-03-15 09:46:38 +0300

    реализовал отображение баланса пока только на одной странице

---
+++ frontend/config/getBalance.js
@@ -0,0 +1,20 @@
+export class GetBalance {
+    static getCommonBalance(operations) {
+        const balanceElement = document.getElementById('common-balance');
+        let totalIncome = 0;
+        let totalExpenses = 0;
+
+        operations.forEach(item => {
+            if (item.type === 'income') {
+                totalIncome += item.amount;
+            } else if (item.type === 'expense') {
+                totalExpenses += item.amount;
+            }
+        });
+
+        // Вычислить баланс
+        const balance = totalIncome - totalExpenses;
+        const formattedBalance = balance.toLocaleString();  // Применение форматирования разделителя разрядов
+        balanceElement.innerText = formattedBalance;
+    }
+}
